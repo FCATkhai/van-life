725273e2b91157d9fca64ba41dd0aac7ffad214e
FCATkhai <fcatkhai@gmail.com>
2023-11-01 15:59:05 +0700

    Add Host Van

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
-import './App.css';
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import "./App.css"
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
 import Home from "./pages/Home"
 import About from "./pages/About"
 import Vans from "./pages/Vans/Vans"
@@ -7,12 +10,17 @@ import VanDetail from "./pages/Vans/VanDetail"
 import Dashboard from "./pages/Host/Dashboard"
 import Income from "./pages/Host/Income"
 import Reviews from "./pages/Host/Reviews"
+import HostVans from "./pages/Host/HostVans"
+import HostVanDetail from "./pages/Host/HostVanDetail"
+import HostVanInfo from "./pages/Host/HostVanInfo"
+import HostVanPricing from "./pages/Host/HostVanPricing"
+import HostVanPhotos from "./pages/Host/HostVanPhotos"
 import Layout from "./components/Layout"
 import HostLayout from "./components/HostLayout"
 
 import "./server"
 
-function App() {
+export default function App() {
   return (
     <BrowserRouter>
       <Routes>
@@ -26,11 +34,15 @@ function App() {
             <Route index element={<Dashboard />} />
             <Route path="income" element={<Income />} />
             <Route path="reviews" element={<Reviews />} />
+            <Route path="vans" element={<HostVans />} />
+            <Route path="vans/:id" element={<HostVanDetail />}>
+              <Route index element={<HostVanInfo />} />
+              <Route path="pricing" element={<HostVanPricing />} />
+              <Route path="photos" element={<HostVanPhotos />} />
+            </Route>
           </Route>
         </Route>
       </Routes>
     </BrowserRouter>
-  );
+  )
 }
-
-export default App;
