7c74db0377451b15bf9fde0dbf72a4c54648400f
FCATkhai <fcatkhai@gmail.com>
2023-12-13 13:54:49 +0700

    add login page
    x button for delete localStorage

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,6 @@
 import React from "react"
 import { Link, NavLink } from "react-router-dom"
+import imageUrl from "../assets/images/avatar-icon.png"
 
 export default function Header() {
     const activeStyles = {
@@ -8,28 +9,39 @@ export default function Header() {
         color: "#161616"
     }
     
+    function fakeLogOut() {
+        localStorage.removeItem("loggedin")
+    }
+    
     return (
         <header>
             <Link className="site-logo" to="/">#VanLife</Link>
             <nav>
                 <NavLink 
-                    to="/host"
+                    to="host"
                     style={({isActive}) => isActive ? activeStyles : null}
                 >
                     Host
                 </NavLink>
                 <NavLink 
-                    to="/about"
+                    to="about"
                     style={({isActive}) => isActive ? activeStyles : null}
                 >
                     About
                 </NavLink>
                 <NavLink 
-                    to="/vans"
+                    to="vans"
                     style={({isActive}) => isActive ? activeStyles : null}
                 >
                     Vans
                 </NavLink>
+                <Link to="login" className="login-link">
+                    <img 
+                        src={imageUrl} 
+                        className="login-icon"
+                    />
+                </Link>
+                <button onClick={fakeLogOut}>X</button>
             </nav>
         </header>
     )
